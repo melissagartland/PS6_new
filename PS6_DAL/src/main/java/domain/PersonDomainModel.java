910f9824779e7d84e2e71f20bb9b65cef4906d3c
melissagartland <magart@udel.edu>
2015-11-12 21:12:55 -0500

    MAG commit

---
+++ PS6_DAL/src/main/java/domain/PersonDomainModel.java
@@ -0,0 +1,114 @@
+package domain;
+
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.UUID;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import util.LocalDateAdapter;
+
+public class PersonDomainModel {
+
+    protected  String firstName;
+    protected  String lastName;
+    protected  String street;
+    protected  int postalCode;
+    protected  String city;
+    protected  Date birthday;
+    private UUID personId;
+
+    /**
+     * Default constructor.
+     */
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String firstNameProperty() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String lastNameProperty() {
+        return lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String streetProperty() {
+        return street;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public int postalCodeProperty() {
+        return postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String cityProperty() {
+        return city;
+    }
+
+    
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Date birthdayProperty() {
+        return birthday;
+    }
+
+	public UUID getPersonId() {
+		return personId;
+	}
+
+	public void setPersonId(UUID personId) {
+		this.personId = personId;
+	}
+}
